f14381bbce8c188f8adaaf037c7e5c61a6456f54
rebecca.murphy <rebeccalorenmurphy@gmail.com>
2013-11-02 15:32:55 -0400

    trying to fix branch

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
-import httplib, urllib, urllib2, demjson, requests, json
-#import simplejson as json 
+import httplib, urllib, urllib2, requests, json
 
 url = 'http://hermes.wha.la/api/hermes'
 headers = {'content-type': 'application/json'}
@@ -17,61 +16,96 @@ start = {
         }
 
 def turn(kind):
-	if kind == 'INIT':
-		response = requests.post(url, data =json.dumps(datainit), headers = headers)
-		text = json.loads(response.text)
-		return text
-	if kind == 'PLAY':
-		response = requests.post(url, data = json.dumps(start), headers = headers)
-		text= json.loads(response.text)
-		return text
-#print response.text
-#print text2["ServerState"]['ServerTiers']['DB']["ServerRegions"]["AP"]['NodeCount']
+        if kind == 'INIT':
+                response = requests.post(url, data =json.dumps(datainit), headers = headers)
+                text = json.loads(response.text)
+                return text
+        if kind == 'PLAY':
+                response = requests.post(url, data = json.dumps(start), headers = headers)
+                text= json.loads(response.text)
+                return text
+
 #pass arrays of 3 [EU, NA, AP] 
 def makechange(WEBNODE, JAVANODE, DBNODE):
-	change =  {'Servers':{
-	               'WEB':{
-	                   'ServerRegions':{
-	                       'EU':{
-	                           'NodeCount': WEBNODE[0]
-	                           },
-	                       'NA':{
-	                           'NodeCount': WEBNODE[1]
-	                           },
-	                       'AP':{
-	                           'NodeCount': WEBNODE[2]
-	                           },
-	                       }
-	                   },
-	               'JAVA':{
-	                       'ServerRegions':{
-	                       	'EU':{
-	                           'NodeCount': JAVANODE[0]
-	                           },
-	                        'NA':{
-	                             'NodeCount': JAVANODE[1]
-	                             },
-	                        'AP':{
-	                             'NodeCount': JAVANODE[2]
-	                             },
-	                           }
-	                       },
-	                'DB':{
-	                       'ServerRegions':{
-	                               'EU':{
-	                           'NodeCount': DBNODE[0]
-	                           },
-	                        'NA':{
-	                             'NodeCount': DBNODE[1]
-	                             },
-	                        'AP':{
-	                             'NodeCount': DBNODE[2]
-	                             },
-	                           }          
-	                     }
-	               },
-	          }
-	changeVals = {'Command': 'CHNG', 'Token' : '3b00ea6d-1383-4fdc-a6a8-9578091947f9','ChangeRequest': change}
-	response = requests.post(url, data= json.dumps(changeVals, sort_keys=False), headers=headers)
-	text2 = json.loads(response.text)
-	return text2
+        change =  {'Servers':{
+                       'WEB':{
+                           'ServerRegions':{
+                               'EU':{
+                                   'NodeCount': WEBNODE[0]
+                                   },
+                               'NA':{
+                                   'NodeCount': WEBNODE[1]
+                                   },
+                               'AP':{
+                                   'NodeCount': WEBNODE[2]
+                                   },
+                               }
+                           },
+                       'JAVA':{
+                               'ServerRegions':{
+                                       'EU':{
+                                   'NodeCount': JAVANODE[0]
+                                   },
+                                'NA':{
+                                     'NodeCount': JAVANODE[1]
+                                     },
+                                'AP':{
+                                     'NodeCount': JAVANODE[2]
+                                     },
+                                   }
+                               },
+                        'DB':{
+                               'ServerRegions':{
+                                       'EU':{
+                                   'NodeCount': DBNODE[0]
+                                   },
+                                'NA':{
+                                     'NodeCount': DBNODE[1]
+                                     },
+                                'AP':{
+                                     'NodeCount': DBNODE[2]
+                                     },
+                                   }          
+                             }
+                       },
+                  }
+        changeVals = {'Command': 'CHNG', 'Token' : '3b00ea6d-1383-4fdc-a6a8-9578091947f9','ChangeRequest': change}
+        response = requests.post(url, data= json.dumps(changeVals, sort_keys=False), headers=headers)
+        text2 = json.loads(response.text)
+        return text2
+
+def addRecords(DBEUrecords, DBNArecords, DBAPrecords, JVEUrecords, JVNArecords, JVAPrecords , WBEUrecords, WBNArecords, WBAPrecords, state):
+        DBEUrecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+        DBNArecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+        DBAPrecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+        
+        JVEUrecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+        JVNArecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+        JVAPrecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+        
+
+        WBEUrecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+        WBNArecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+        WBAPrecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+
+
+def linreg(X, Y):
+    """
+    X is a list, y is the length
+    return a,b in solution to y = ax + b such that root mean square distance between trend line and original points is minimized
+    y = mx+b
+    """
+    N = len(X)
+    Sx = Sy = Sxx = Syy = Sxy = 0.0
+    for x, y in map(None, X, Y):
+        Sx = Sx + x
+        Sy = Sy + y
+        Sxx = Sxx + x*x
+        Syy = Syy + y*y
+        Sxy = Sxy + x*y
+    det = Sxx * N - Sx * Sx
+    return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
+
+def rateofchange(records):
+        m,b = linreg(range(len(records)),records) #your x,y are switched from standard notation
+        return m
--- pythontest.py
+++ pythontest.py
@@ -1,6 +1,36 @@
-import httplib, urllib, urllib2, demjson, requests, functions, json
-#import simplejson as json 
+import httplib, urllib, urllib2, requests, functions, json
 
-print functions.turn("INIT")
-print functions.turn("PLAY")["ServerState"]['ServerTiers']['DB']["ServerRegions"]["AP"]['NodeCount']
-print functions.makechange([0,0,0], [0,0,0], [0,0,0])
+DBEUrecords= []
+DBNArecords= []
+DBAPrecords= []
+JVEUrecords=[]
+JVNArecords=[]
+JVAPrecords =[]
+WBEUrecords=[]
+WBNArecords=[]
+WBAPrecords=[]
+
+functions.turn("INIT")
+
+
+#print functions.makechange([0,0,0], [0,0,0], [0,0,0])
+# 
+i=0
+while (i<1000):
+        state = functions.turn("PLAY")
+        functions.addRecords(DBEUrecords,DBNArecords,DBAPrecords, JVEUrecords, JVNArecords, JVAPrecords , WBEUrecords, WBNArecords, WBAPrecords, state)
+        #if WBEUrecords[i] != WBEUrecords[ i-1] and i >=1:
+        #        print i
+        #        break
+
+        if (i  %25==0 and i !=0):
+                print functions.rateofchange(WBNArecords[i-25:i])
+                if (functions.rateofchange(WBNArecords[i-25:i]) > 1):
+                        functions.makechange([0,1,0], [0,0,0], [0,0,0])
+                elif (functions.rateofchange(WBNArecords[i-25:i]) <1 and state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["NA"]['NodeCount'] != 1):
+                        functions.makechange([0,-1,0], [0,0,0], [0,0,0])
+        i+= 1
+
+#print DBEUrecords these only start changing after 1000 turns.
+#JVEUrecords takes 829 turns
+#10,080
