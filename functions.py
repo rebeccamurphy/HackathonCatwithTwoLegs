f8f11fd1aebf9f69436b26c18e0839053ac6219b
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-11-02 15:17:00 -0400

    messing with stuff

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
-import httplib, urllib, urllib2, demjson, requests, json
-#import simplejson as json 
+import httplib, urllib, urllib2, requests, json
 
 url = 'http://hermes.wha.la/api/hermes'
 headers = {'content-type': 'application/json'}
@@ -25,8 +24,7 @@ def turn(kind):
 		response = requests.post(url, data = json.dumps(start), headers = headers)
 		text= json.loads(response.text)
 		return text
-#print response.text
-#print text2["ServerState"]['ServerTiers']['DB']["ServerRegions"]["AP"]['NodeCount']
+
 #pass arrays of 3 [EU, NA, AP] 
 def makechange(WEBNODE, JAVANODE, DBNODE):
 	change =  {'Servers':{
@@ -75,3 +73,39 @@ def makechange(WEBNODE, JAVANODE, DBNODE):
 	response = requests.post(url, data= json.dumps(changeVals, sort_keys=False), headers=headers)
 	text2 = json.loads(response.text)
 	return text2
+
+def addRecords(DBEUrecords, DBNArecords, DBAPrecords, JVEUrecords, JVNArecords, JVAPrecords , WBEUrecords, WBNArecords, WBAPrecords, state):
+	DBEUrecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+	DBNArecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+	DBAPrecords.append(state["ServerState"]['ServerTiers']['DB']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+	
+	JVEUrecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+	JVNArecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+	JVAPrecords.append(state["ServerState"]['ServerTiers']['JAVA']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+	
+
+	WBEUrecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["EU"]['NoOfTransactionsInput'])
+	WBNArecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["NA"]['NoOfTransactionsInput'])
+	WBAPrecords.append(state["ServerState"]['ServerTiers']['WEB']["ServerRegions"]["AP"]['NoOfTransactionsInput'])
+
+
+def linreg(X, Y):
+    """
+    X is a list, y is the length
+    return a,b in solution to y = ax + b such that root mean square distance between trend line and original points is minimized
+    y = mx+b
+    """
+    N = len(X)
+    Sx = Sy = Sxx = Syy = Sxy = 0.0
+    for x, y in map(None, X, Y):
+        Sx = Sx + x
+        Sy = Sy + y
+        Sxx = Sxx + x*x
+        Syy = Syy + y*y
+        Sxy = Sxy + x*y
+    det = Sxx * N - Sx * Sx
+    return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
+
+def rateofchange(records):
+	m,b = linreg(range(len(records)),records) #your x,y are switched from standard notation
+	return m
